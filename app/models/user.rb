d5ef6b6d0e2accf29498f2802400103baab18663
Derrick Ellerbie <dellerbie@gmail.com>
2013-03-04 19:17:29 -0800

    Updated the select inputs to use the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ActiveRecord::Base
   
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :listing_attributes
   
+  validates_presence_of :first_name
+  validates_length_of :first_name, maximum: 255
+
+  validates_presence_of :last_name
+  validates_length_of :last_name, maximum: 255
+  
   def after_initialize
     self.build_listing if self.listing.nil?
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of :first_name
+  should ensure_length_of(:first_name).is_at_most(255)
+  
+  should validate_presence_of :last_name
+  should ensure_length_of(:last_name).is_at_most(255)
+  
+  should validate_presence_of :email
+  should allow_value("d@d.com").for(:email)
+  should_not allow_value('d.com').for(:email)
+  
+  should validate_presence_of :password
 end
