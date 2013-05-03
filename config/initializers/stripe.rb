c91e35d4be3c398135fca026e6585631c4d6e432
Derrick Ellerbie <dellerbie@gmail.com>
2013-05-03 10:33:46 -0700

    Testing stripe webhook in heroku

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,10 +3,14 @@ STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
 
 StripeEvent.setup do
   subscribe 'customer.subscription.deleted' do |event|
+    puts "Received Stripe Event 'customer.subscription.deleted'"
+    p event
     StripeWebhooks.subscription_canceled(event.data.object.customer)
   end
   
   subscribe 'customer.subscription.created' do |event|
+    puts "Received Stripe Event 'customer.subscription.created'"
+    p event
     StripeWebhooks.subscription_created(event.data.object.customer)
   end
 end
