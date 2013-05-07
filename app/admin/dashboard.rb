8f5e63c39732a827fc9633236820bcdaa21887bd
Derrick Ellerbie <dellerbie@gmail.com>
2013-05-07 16:51:38 -0700

    Admin dashboard showing stats

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,29 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+
+    panel 'Stats' do 
+      div do 
+        "#{User.count(conditions: {premium: true})} Premium Users"
+      end
+    
+      div do 
+        "#{User.count} Total Users"
+      end
+    
+      div do 
+        "#{Listing.count} Total Listings"
+      end
+      
+      div do 
+        "#{Listing.count(conditions: {claimable: true})} Claimable Listings"
+      end
+    
+      div do 
+        "#{Question.count} Total Questions"
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
