46fd26cb9ac920be7441e88fa337653fe1fa8708
Derrick Ellerbie <dellerbie@gmail.com>
2013-05-15 15:40:56 -0700

    Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(:version => 20130508233936) do
     t.boolean  "claimable",                   :default => false
   end
 
-  add_index "listings", ["city_id"], :name => "index_listings_on_city_id"
   add_index "listings", ["slug"], :name => "index_listings_on_slug", :unique => true
   add_index "listings", ["user_id"], :name => "index_listings_on_user_id", :unique => true
 
