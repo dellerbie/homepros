6a14502bb2711281855881a994fd1afd96a815f8
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-04 15:07:50 -0700

    Show listing spec finished

--- spec/features/show_listing_spec.rb
+++ spec/features/show_listing_spec.rb
@@ -2,10 +2,55 @@ require 'spec_helper'
 
 feature 'Listing show view' do 
   
-  scenario 'claimable listing' do 
-    
+  def should_be_a_contactable_listing(listing)
+    page.should have_content('Ask this business a question')
+    page.should have_content('Contact Us')
+    page.should have_content("Email #{listing.company_name}")
   end
   
-  scenario 'claimed listing'
-  scenario "signed in user's listing"
+  def should_not_be_a_contactable_listing(listing)
+    page.should_not have_content('Ask this business a question')
+    page.should_not have_content('Contact Us')
+    page.should_not have_content("Email #{listing.company_name}")
+  end
+  
+  scenario 'claimable listing' do 
+    listing = FactoryGirl.create(:claimable_listing)
+    visit listing_path(listing)
+    
+    page.should have_content(listing.company_name)
+    page.should have_content('Claim this listing')
+    page.should have_content("Specialties: #{listing.specialties.map(&:name).join(', ')}")
+    page.should have_content("Headquarted in: #{listing.city.name}, CA")
+    page.should have_content('Back to Listings')
+    page.should have_content('Website')
+    page.should have_content(listing.website)
+    
+    should_be_a_contactable_listing(listing)
+  end
+  
+  scenario 'with no contact information' do
+    listing = FactoryGirl.create(:claimable_listing, contact_email: Listing::NO_CONTACT_EMAIL, 
+                website: Listing::NO_WEBSITE, phone: Listing::NO_PHONE)
+    visit listing_path(listing)
+    should_not_be_a_contactable_listing(listing)
+    page.should_not have_content('Website')
+    page.should_not have_content(listing.website)
+  end
+  
+  scenario 'claimed listing' do 
+    listing = FactoryGirl.create(:free_listing)
+    visit listing_path(listing)
+    page.should_not have_content('Claim this listing')
+  end
+  
+  scenario "signed in user's listing" do 
+    listing = FactoryGirl.create(:free_listing)
+    sign_in(listing.user)
+    visit listing_path(listing)
+    
+    page.should have_content(listing.company_name)
+    page.should_not have_content('Claim this listing')
+    should_not_be_a_contactable_listing(listing)
+  end
 end
