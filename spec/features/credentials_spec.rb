a1fa60452696345554577dd4f82e01394f1a3f54
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-04 20:09:12 -0700

    Credentials spec beginnings

--- spec/features/credentials_spec.rb
+++ spec/features/credentials_spec.rb
@@ -2,7 +2,29 @@ require 'spec_helper'
 
 feature 'Credentials', js: true do
   
-  scenario 'login'
+  before(:each) do 
+    @user = FactoryGirl.create(:free_listing).user
+  end
+  
+  scenario 'login' do 
+    visit new_user_session_path
+    fill_in "Email", :with => @user.email
+    fill_in "Password", :with => @user.password
+    click_button "Sign in"
+    
+    should_see_signed_in_navbar_for_mvp_user
+    page.should have_content('Signed in successfully.')
+    current_path.should eql(root_path)
+  end
+  
+  scenario 'unsuccessful login' do
+    visit new_user_session_path
+    click_button "Sign in"
+    
+    page.should have_content('Invalid email or password.')
+    current_path.should eql(new_user_session_path)
+  end
+  
   scenario 'forgot password'
   
 end
