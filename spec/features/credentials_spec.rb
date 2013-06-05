faf5e4774f25ff15600805613529c66abc760aac
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-05 12:07:42 -0700

    Finished the credentials spec

--- spec/features/credentials_spec.rb
+++ spec/features/credentials_spec.rb
@@ -25,6 +25,25 @@ feature 'Credentials', js: true do
     current_path.should eql(new_user_session_path)
   end
   
-  scenario 'forgot password'
+  scenario 'forgot password' do
+    visit new_user_session_path
+    click_on "Forgot your password?"
+    
+    fill_in 'user_email', with: @user.email
+    click_on 'Send me reset password instructions'
+    page.should have_content('You will receive an email with instructions about how to reset your password in a few minutes.')
+    current_path.should eql(new_user_session_path)
+  end
+  
+  scenario 'forgot password, user not found' do
+    visit new_user_session_path
+    click_on "Forgot your password?"
+    
+    fill_in 'user_email', with: 'unknownuser@gmail.com'
+    click_on 'Send me reset password instructions'
+    
+    page.should have_content('Please review the problems below:')
+    page.should have_css('.user_email.error')
+  end
   
 end
