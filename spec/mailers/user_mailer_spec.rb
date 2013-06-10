e4d3c85e4014a49212b0eb950b55fb3f2edf6a7f
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-09 19:40:58 -0700

    Working welcome to premium emails

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -20,35 +20,44 @@ describe UserMailer do
     it 'has the correct subject' do
       mail.subject.should == subject
     end
+    
+    it 'has content' do 
+      mail.should have_body_text(text)
+    end
   end
   
   describe '#welcome_email' do
     let(:mail) { UserMailer.welcome_email(user) }
     let(:subject) { '[OC HomeMasters] Welcome!' }
+    let(:text) { 'Welcome to OC HomeMasters!' }
     it_behaves_like 'oc homepros user mailer'
   end
   
   describe '#downgrade_email' do
     let(:mail) { UserMailer.downgrade_email(user) }
     let(:subject) { '[OC HomeMasters] Your Listing Has Been Downgraded' }
+    let(:text) { 'has been downgraded to a free listing.' }
     it_behaves_like 'oc homepros user mailer'
   end
   
   describe '#welcome_to_premium_email' do
     let(:mail) { UserMailer.welcome_to_premium_email(user) }
     let(:subject) { '[OC HomeMasters] Welcome to Premium Listings!' }
+    let(:text) { 'has been upgraded to a Premium Listing.' }
     it_behaves_like 'oc homepros user mailer'
   end
   
   describe '#payment_receipt_email' do
     let(:mail) { UserMailer.payment_receipt_email(user) }
     let(:subject) { '[OC HomeMasters] Payment Receipt' }
+    let(:text) { "This is a receipt for your OC HomeMaster's premium listing." }
     it_behaves_like 'oc homepros user mailer'
   end
   
   describe '#payment_failed_email' do
     let(:mail) { UserMailer.payment_failed_email(user) }
     let(:subject) { '[OC HomeMasters] Declined Payment' }
+    let(:text) { 'We have been unable to charge your credit card for your premium listing' }
     it_behaves_like 'oc homepros user mailer'
   end
 end
