2a4909db8ae7a9c933da1f2528d00e2b95b92685
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-12 14:34:12 -0700

    Fix styles in IE9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  after_filter :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = request.protocol + ENV['APP_HOST']
-  end
 end
