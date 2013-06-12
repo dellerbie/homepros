03157dfe82053ed32e80ccdab812ef08b08f2e0c
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-12 13:15:09 -0700

    Allow CORS for webfonts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = request.protocol + ENV['APP_HOST']
+  end
 end
