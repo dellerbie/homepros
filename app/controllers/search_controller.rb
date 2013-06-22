4f4345fe4c10e6c410261a0ab430a93d1db7bc9f
Derrick Ellerbie <dellerbie@gmail.com>
2013-06-21 17:49:07 -0700

    Redirect to the homepage if the search query is blank

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,10 @@ class SearchController < ApplicationController
   skip_filter :hide_search
   
   def index
+    redirect_to root_path if params[:q].blank?
+    
     @listings = []
-    @listings = Listing.search_by_company_name(params[:q]).paginate(page: 1, per_page: 30) if params[:q].present?
+    @listings = Listing.search_by_company_name(params[:q]).paginate(page: 1, per_page: 30)
     @total = @listings.total_entries
     @listings = @listings.sort_by { |l| l.premium? ? 0 : 1 } 
     @listings.take((1..10).to_a.sample).each { |listing| listing.premium = true }
